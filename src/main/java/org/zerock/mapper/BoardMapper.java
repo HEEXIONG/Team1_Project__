1c3d596579a0686ca423f2ab7f19a165dfbeaa3b
qoqbobb <kail853@naver.com>
2022-08-18 19:54:48 +0900

    리스트까지

--- src/main/java/org/zerock/mapper/BoardMapper.java
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -1,5 +1,8 @@
 package org.zerock.mapper;
 
+
+import java.util.List;
+
 import org.zerock.domain.BoardVO;
 
 public interface BoardMapper {
@@ -7,5 +10,7 @@ public interface BoardMapper {
 	void insert(BoardVO vo);
 	
 	void update(BoardVO vo);
+	
+	List<BoardVO> getlist(); 
 
 }
